fdc255131a92de2887844a6106eb94e11756726c
권현빈 <43259318+bini0823@users.noreply.github.com>
2020-09-03 13:36:42 +0900

    Add files via upload
    dynamic programming 이항계수 (4,2) 구하기

---
+++ chapter2/BinomialCoefficient.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int A[100][100] = { 0, };
+	for (int i = 1; i < 5; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (i == j || j == 0) {
+				A[i][j] = 1;
+			}
+			else {
+				A[i][j] = A[i - 1][j - 1] + A[i - 1][j];
+			}
+		}
+	}
+
+	printf("%d", A[4][2]);
+}
+
